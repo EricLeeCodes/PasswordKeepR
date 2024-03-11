413d66f769b3f2b42a34f0cb12e4822181b8435d
Eric Lee <ericleecodes@gmail.com>
2024-03-11 12:50:04 -0700

    Add email field

--- db/schema/05_accounts.sql
+++ db/schema/05_accounts.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS accounts CASCADE;
 
 CREATE TABLE accounts (
   id SERIAL PRIMARY KEY NOT NULL,
+  email VARCHAR(50) NOT NULL,
   password VARCHAR(20) NOT NULL,
   site_id INTEGER REFERENCES sites(id),
   category_id INTEGER REFERENCES category(id),
