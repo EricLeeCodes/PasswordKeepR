8f50b1774df7da39df47311b6475f33c03701868
Eric Lee <ericleecodes@gmail.com>
2024-03-13 17:17:23 -0700

    Route css to pages

--- express-server.js
+++ express-server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 // Configuration
 
@@ -15,6 +16,8 @@ app.set('view engine', 'ejs');
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+console.log(__dirname);
 
 //Route
 
